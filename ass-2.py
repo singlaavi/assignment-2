3bec2a6d4bcc8ac6b71299b57b26a7c68dfdbe6c
singlaavi <avi.singla16@gmail.com>
2018-08-17 00:58:36 +0530

    COMMIT

---
+++ ass-2.py
@@ -0,0 +1,33 @@
+#SOLUTION 1
+print('ASSIGNMENT -2')
+
+#SOLUTION 2
+a=input()
+b=input()
+print(a+b)
+
+#SOLUTION 3
+x=int(input())
+y=int(input())
+z=int(input())
+print(x,y,z)
+
+#SOLUTION 4
+print("Let's get started")
+
+#SOLUTION 5
+s="Acadview"
+course="Python"
+fees=5000
+#As they said to print using placeholders
+print('%s' %(s))
+print('%s' %(course))
+print('%d' %(fees))
+
+#SOLUTION 6 INTRESTING THING
+name="Tony Stark"
+salary=1000000
+#we have to apply brackets around print statement
+print(('%s''%d')%(name,salary))
+
+
